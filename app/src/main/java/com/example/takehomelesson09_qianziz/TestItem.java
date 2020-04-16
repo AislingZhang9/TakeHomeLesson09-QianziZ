e89a84b982c30edd880c5e428bef0e55e218f857
aisling <aisling@AislingdeMacBook-Air-2.local>
2020-04-16 13:29:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/takehomelesson09_qianziz/TestItem.java
@@ -0,0 +1,20 @@
+package com.example.takehomelesson09_qianziz;
+
+public class TestItem {
+
+    String question;
+    String answer;
+
+    public TestItem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+    public String getQuestion(){
+        return question;
+    }
+
+    public String getAnswer(){
+        return answer;
+    }
+
+}
